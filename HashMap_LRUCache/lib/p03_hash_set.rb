0977348225753974649bab84233127beb235abd4
Nicholas <nwilliams770@gmail.com>
2017-12-27 14:18:37 -0800

    hasset passing

--- HashMap_LRUCache/lib/p03_hash_set.rb
+++ HashMap_LRUCache/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    if self.include?(key)
+      return false
+    else
+      self[key.hash] << key
+      @count += 1
+      resize! if num_buckets < @count
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return nil unless self.include?(key)
+    self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,10 @@ class HashSet
   end
 
   def resize!
+    prev_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    @count = 0
+    prev_store.flatten.each { |num| insert(num) }
   end
 end
