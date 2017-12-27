a03f44fd201034de2fbb9937fb061ca62e9af3e4
Nicholas <nwilliams770@gmail.com>
2017-12-27 13:22:27 -0800

    int set rspec passing

--- HashMap_LRUCache/lib/p01_int_set.rb
+++ HashMap_LRUCache/lib/p01_int_set.rb
@@ -41,18 +41,23 @@ class IntSet
   end
 
   def insert(num)
+    return false if self.include?(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
