480de00a00cd0e5f8db4810fe18bc089eb303754
Nicholas <nwilliams770@gmail.com>
2017-12-27 16:44:35 -0800

    linkedlist specs passing

--- HashMap_LRUCache/lib/p04_linked_list.rb
+++ HashMap_LRUCache/lib/p04_linked_list.rb
@@ -15,11 +15,31 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    #connect prev and next nodes to each other
+    if self.prev
+      self.prev.next = self.next
+    end
+
+    if self.next
+      self.next.prev = self.prev
+    end
+    
+    # remove current node from pointing to any other nodes e.g. 'removed'
+    self.next = nil
+    self.prev = nil
+    
+
   end
 end
 
 class LinkedList
+  include Enumerable
+  
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +48,64 @@ class LinkedList
   end
 
   def first
+    @head.next unless empty?
   end
 
   def last
+    @tail.prev unless empty?
   end
 
   def empty?
+    @tail.prev == @head
   end
 
   def get(key)
+    self.each { |node| return node.val if node.key == key}
+    nil
   end
 
   def include?(key)
+    self.any? { |node| node.key == key }
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+
+    @tail.prev.next = new_node
+    new_node.prev = @tail.prev
+
+    new_node.next = @tail
+    @tail.prev = new_node
+    
+    new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
+    nil
   end
 
   def each
+    working_node = @head.next
+
+    until working_node == @tail
+      yield working_node
+      working_node = working_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
