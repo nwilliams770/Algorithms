f214914f5352ea240417164d94dc1b7f70f83786
Nicholas <nwilliams770@gmail.com>
2017-12-28 12:06:43 -0800

    dynamic array methods added

--- Dynamic_Arrays_Project/lib/dynamic_array.rb
+++ Dynamic_Arrays_Project/lib/dynamic_array.rb
@@ -1,7 +1,7 @@
 require_relative "static_array"
 
 class DynamicArray
-  attr_reader :length
+  include Enumerable
 
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
@@ -14,40 +14,65 @@ class DynamicArray
 
   # O(1)
   def [](index)
+   check_index(index)
    @store[index]
   end
 
 
   # O(1)
   def []=(index, value)
+    check_index
+    @store[index] = value
   end
 
   # O(1)
   def pop
-
+    return nil if @count == 0
+    last_item = @store[(@count - 1) % capacity]
+    @count =- 1
+    last_item
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if capacity == @count
+    @store[@count % capacity] = val
+    @count += 1
+    self
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    return nil if @count == 0
+    first_item = @store[0]
+    @count =- 1
+    first_item
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if capacity == @count
+
   end
 
 
   def check_index(index)
     if index > capacity
-      raise Exception.new("index out of bounds")
+      raise "index out of bounds"
     end
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    new_store = StaticArray.new(capacity * 2)
+    self.each_with_index { |el, i| new_store[i] = el}
+
+    @store = new_store
+  end
+
+  def each
+    @count.times { |i| yield self[i] }
+    self
   end
 end
