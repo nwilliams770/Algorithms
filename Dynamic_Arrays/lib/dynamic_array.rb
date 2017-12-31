5e33c655454288069bec17340ac9659d1d565e7e
Nicholas <nwilliams770@gmail.com>
2017-12-30 21:11:08 -0800

    dynamic array

--- Dynamic_Arrays/lib/dynamic_array.rb
+++ Dynamic_Arrays/lib/dynamic_array.rb
@@ -3,76 +3,76 @@ require_relative "static_array"
 class DynamicArray
   include Enumerable
 
+  attr_reader :length
+
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
-    @count = 0
-  end
-
-  def capacity
-    @store.length
+    @length = 0
+    @capacity = capacity
   end
 
   # O(1)
   def [](index)
-   check_index(index)
-   @store[index]
+    check_index(index)
+    @store[index]
   end
-
 
   # O(1)
   def []=(index, value)
-    check_index
+    check_index(index)
     @store[index] = value
   end
 
   # O(1)
   def pop
-    return nil if @count == 0
-    last_item = @store[(@count - 1) % capacity]
-    @count =- 1
-    last_item
+    check_index(0)
+    @length =- 1
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
-    resize! if capacity == @count
-    @store[@count % capacity] = val
-    @count += 1
-    self
+    resize! if @capacity == @length
+    @store[@length] = val
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
-    return nil if @count == 0
-    first_item = @store[0]
-    @count =- 1
-    first_item
+    check_index(@length)
+    @length =- 1
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
-    resize! if capacity == @count
-
+    resize! if @capacity == @length
+    self.each_with_index { |el, i| @store[i + 1]= el}
+    @store[0] = val
+    @length += 1
   end
 
+  protected
+  attr_accessor :capacity, :store
+  attr_writer :length
 
   def check_index(index)
-    if index > capacity
+    if index - 1 > @length || index < 0
       raise "index out of bounds"
     end
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
-    new_store = StaticArray.new(capacity * 2)
+    new_capacity = @capacity * 2
+    new_store = StaticArray.new(new_capacity)
     self.each_with_index { |el, i| new_store[i] = el}
 
+    @capacity = new_capacity
     @store = new_store
   end
 
   def each
-    @count.times { |i| yield self[i] }
+    @length.times { |i| yield self[i] }
     self
   end
 end
